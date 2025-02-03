62e78e244f8fa5e8fbaba4fe5dde6ca4eae4e588
M.Ngugi <mngugi7@gmail.com>
2025-02-03 23:35:20 +0300

    usingIrb.rb

---
+++ usingIrb.rb
@@ -0,0 +1,6 @@
+irb
+require 'tk'
+require 'tkextlib/tile'
+root = TkRoot.new() 
+button = Tk::Tile::TButton.new(root) {text "Hello World"}.grid
+Tk.mainloop()
---
+++ usingIrb.tcl
@@ -0,0 +1,5 @@
+require 'tk'
+require 'tkextlib/tile'
+root = TkRoot.new() 
+button = Tk::Tile::TButton.new(root) {text "Hello World"}.grid
+Tk.mainloop()

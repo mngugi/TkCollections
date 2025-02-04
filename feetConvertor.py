95069cf2c2b68f4175e8334dcc35b80dde4fbd4c
M.Ngugi <mngugi7@gmail.com>
2025-02-04 20:15:08 +0300

    Update feetConvertor.py

--- feetConvertor.py
+++ feetConvertor.py
@@ -11,7 +11,7 @@ def calculate(*args):
 root = Tk()
 root.title("Feet to Meters")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe = ttk.Frame(root, padding="5 5 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
